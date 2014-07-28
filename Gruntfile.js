0ed4cd6a13e62136d5e728913c28f449f1997a50
Benoit RUIZ <bruiz@teads.tv>
2014-07-28 15:03:47 +0200

    Merge branch 'feat-MAN-235' into 'master'
    Close button is too small on modals (please ask OR/BLO if necessary)
    
    Conflicts:
    	Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,9 +52,6 @@ module.exports = function (grunt) {
 
   });
 
-  grunt.registerTask('new_feat', ['ttdev:create:feat']);
-  grunt.registerTask('end_feat', ['ttdev:finish:feat']);
-  grunt.registerTask('new_fix', ['ttdev:create:fix']);
-  grunt.registerTask('end_fix', ['ttdev:finish:fix']);
+  // ADDED
 
 };
