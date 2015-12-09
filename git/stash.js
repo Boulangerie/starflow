d6f36a7213ac1f3ca89473ce1c812f1785c661e7
Benoit Ruiz <benoit.ruiz@teads.tv>
2015-12-09 12:27:19 +0100

    Better success/error handling for getStashId from git.stash

--- git/stash.js
+++ git/stash.js
@@ -5,6 +5,7 @@ var Task = require('../Task');
 var spawnFactory = require('../shell/spawn');
 
 var STASH_NAME = 'starflow-tmp';
+var STASH_ID_UNDEFINED_MESSAGE = 'Could not find any stash ID for starflow-tmp';
 
 function Stash() {
 
@@ -26,26 +27,42 @@ Stash.prototype.getStashId = function getStashId() {
           }
         }
       }
+
+      if (_.isUndefined(flow.git) || (flow.git && _.isUndefined(flow.git.starflowTmpStashId))) {
+        throw new Error(STASH_ID_UNDEFINED_MESSAGE);
+      }
+
       return flow;
     });
 };
 
 Stash.prototype.stash = function stash(isPop) {
-  function onError(err) {
+  function onStashError(err) {
     if (!/No stash found/.test(err.message)) {
       throw err;
     }
+    starflow.logger.warning('Nothing was stashed');
+    return starflow.flow;
+  }
+
+  function onGetStashIdSuccess(flow) {
+    var gitArgs = ['stash', (isPop ? 'pop' : 'save'), (isPop ? 'stash@{' + flow.git.starflowTmpStashId + '}' : STASH_NAME)];
+    return new Task(spawnFactory(), ['git', gitArgs], '$').run();
+  }
+
+  function onGetStashIdError(err) {
+    if (err.message !== STASH_ID_UNDEFINED_MESSAGE) {
+      throw err;
+    }
+    starflow.logger.warning('No starflow-tmp stash was found');
     return starflow.flow;
   }
 
   var promise = isPop ? this.getStashId.bind(this) : Q.bind(starflow.flow);
 
   return promise()
-    .then(function (flow) {
-      var gitArgs = ['stash', (isPop ? 'pop' : 'save'), (isPop ? 'stash@{' + flow.git.starflowTmpStashId + '}' : STASH_NAME)];
-      return new Task(spawnFactory(), ['git', gitArgs], '$').run();
-    })
-    .catch(onError);
+    .then(onGetStashIdSuccess, onGetStashIdError)
+    .catch(onStashError);
 };
 
 Stash.prototype.exec = function exec(isPop) {
