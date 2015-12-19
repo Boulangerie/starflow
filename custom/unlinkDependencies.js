235358b838ea3bebda4d514e0ec8c806af18a58a
Yannick Galatol <yannick.galatol@teads.tv>
2015-12-19 20:51:58 +0100

    Implement linking and unlinking of one or several NPM dependencies That task does not include the git checkout part and should be implemented in another task

--- custom/unlinkDependencies.js
+++ custom/unlinkDependencies.js
@@ -2,13 +2,14 @@ var _ = require('lodash');
 var Q = require('q');
 var starflow = require('../starflow');
 var Task = require('../Task');
+var Sequence = require('../Sequence');
 var spawnFactory = require('../shell/spawn');
 
-function LinkDependencies() {
+function UnlinkDependencies() {
 
 }
 
-LinkDependencies.prototype.exec = function () {
+UnlinkDependencies.prototype.exec = function () {
 
   var dependencies = _.toArray(arguments);
   var dependencyChainSeparator = '/';
@@ -30,18 +31,21 @@ LinkDependencies.prototype.exec = function () {
   });
 
   var promises = _.map(deps, function (dep) {
-    return new Task(spawnFactory(), ['npm', 'link', dep.name]).run();
+    return new Sequence([
+      new Task(spawnFactory(), ['npm', 'unlink', dep.name]),
+      new Task(spawnFactory(), ['npm', 'install', dep.name])
+    ]).run();
   });
 
   return Q.all(promises)
     .then(function () {
-      starflow.logger.log('NPM dependencies linked: ' + _.pluck(deps, 'name').join(', '));
+      starflow.logger.log('NPM dependencies unlinked: ' + _.pluck(deps, 'name').join(', '));
     })
     .catch(function () {
-      starflow.logger.warning('Could not link the dependencies');
+      starflow.logger.warning('Could not unlink the dependencies');
     });
 };
 
 module.exports = function () {
-  return new LinkDependencies();
+  return new UnlinkDependencies();
 };
