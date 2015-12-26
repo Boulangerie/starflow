3bbf7977888f35623532a9a5d864becffb05782e
julien-vidal <julien.vidal.jv@gmail.com>
2015-12-26 17:37:51 +0100

    feat(GH17) Create an echo task under starflow-shell
    - A bit easier than always map to a shell task which can't support easily json
    parsing. Created for debugging purpose for now.
    - Allow to echo a value from a variable interpolated
    - Allow to echo a value from the starflow configuration
    
    echo from interpolated : {'echo': ['{{jira.issue.fields.issuetype.name}}']}
    echo from config : {'echo': ['jira.issue.fields.issuetype.name', 'fillToUse1stAsConfigKey']}
    
    Could be improved a lot, idea are welcome.
    Not a shell service, maybe it's time to think about an other section for this kind
    of tasks

---
+++ shell/echo.js
@@ -0,0 +1,18 @@
+var _ = require('lodash');
+var starflow = require('../starflow');
+
+function Echo() {}
+
+//Implement Executable interface just means declare an exec function here.
+Echo.prototype.exec = function exec(message, fromStarflowConfig) {
+  var formattedMessage = message;
+  fromStarflowConfig = !_.isUndefined(fromStarflowConfig);
+  if(fromStarflowConfig){
+    formattedMessage = JSON.stringify(_.get(starflow.config, message), ' ', 2);
+  }
+  starflow.logger.log(formattedMessage);
+};
+
+module.exports = function () {
+  return new Echo();
+};
