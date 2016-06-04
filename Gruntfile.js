33dba09f1c4b5840df50db99ce288eba28175d0a
Benoit Ruiz <benoit.ruiz01@gmail.com>
2016-06-04 13:47:28 +0200

    Add grunt-bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,15 @@
 module.exports = function (grunt) {
   'use strict';
 
-  grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-bump');
 
   grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
-
-    tdw: {
-      options: {
-        branchTpl: '{{ issueType }}/{{ issueKey }}/{{ issueSlug }}',
-        jira: {
-          protocol: 'https',
-          host: 'jira.ebuzzing.com',
-          projectKey: 'MAN'
-        },
-        gitlab: {
-          protocol: 'https',
-          host: 'git.teads.tv',
-          projectName: 'SERVICE Manager'
+      bump: {
+        options: {
+          pushTo: 'origin'
         }
-      },
-      mr: [
-        { 'createMergeRequest': ['test-dev-workflow', 'master'] }
-      ],
-      c: [ // create
-        //{ 'gitCheckout': ['master'] },
-        //{ 'gitFetch': ['origin', 'master'] },
-        //{ 'gitRebase': ['origin/master', 'master'] },
-        { 'getJiraIssue': [] },
-        //{ 'createBranch': [] },
-        //{ 'gitCheckout': [] },
-        //{ 'gitPush': ['origin'] },
-        //{ 'gitlabCreateMergeRequest': [null, 'master'] }
-        { 'assignMergeRequest': ['ygalatol'] }
-        //{ 'createBranch': [] },
-        //{ 'gitCheckout': [] }
-        // { 'git.checkout': { branch: 'master' } }
-        // { 'git.pull': { remote: 'origin', branch: 'master', withRebase: true } },
-        // { 'git.createBranch': { withCheckout: true } },
-        // 'git.push',
-        // { 'gitlab.createMergeRequest': { refBranch: 'master' } },
-        // 'jira.assignIssue',
-        // { 'jira.changeIssueStatus': { status: 'In  Progress' } }
-      ],
-      f: [ // finish
-        // { 'gitlab.assignMergeRequest': { assignee: 'ygalatol' } },
-        // { 'jira.changeIssueStatus': { status: 'TO REVIEW' } }
-      ]
-    }
+      }
 
   });
 
