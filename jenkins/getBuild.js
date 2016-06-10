2f97927329bcbb7a029ae113bf4e979718c1c617
Yannick Galatol <yannick.galatol@teads.tv>
2016-06-11 00:10:32 +0200

    Create an executable getBuild to get Jenkins build information

---
+++ jenkins/getBuild.js
@@ -0,0 +1,26 @@
+var jenkinsService = require('./jenkinsService');
+var starflow = require('../starflow');
+var BaseExecutable = require('../Executable');
+
+function GetBuild() {
+  BaseExecutable.call(this, 'jenkins.getBuild');
+}
+
+GetBuild.prototype = Object.create(BaseExecutable.prototype);
+GetBuild.prototype.constructor = GetBuild;
+
+GetBuild.prototype.exec = function exec(jobName, buildNumber) {
+  return jenkinsService.build.get(jobName, buildNumber)
+    .then(function (data) {
+      starflow.logger.success('Build information of #' + buildNumber + ' got successfully');
+      this.storage.set('build', data);
+    }.bind(this), function (err) {
+      starflow.logger.error('Could not get build information ' + err);
+      throw err;
+    });
+
+};
+
+module.exports = function () {
+  return new GetBuild();
+};
